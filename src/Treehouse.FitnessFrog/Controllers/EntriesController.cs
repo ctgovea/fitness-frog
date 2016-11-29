742cb0b5e84305a7d7e22edc0e408d6ff1e4e48c
TorresC5 <ctgovea@gmail.com>
2016-11-29 17:25:00 -0600

    Added confirmation messages

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -63,6 +63,8 @@ namespace Treehouse.FitnessFrog.Controllers
             {
                 _entriesRepository.AddEntry(entry);
 
+                TempData["Message"] = "Your entry was succesfully added!";
+
                 return RedirectToAction("Index");
             }
 
@@ -106,6 +108,8 @@ namespace Treehouse.FitnessFrog.Controllers
             {
                 _entriesRepository.UpdateEntry(entry);
 
+                TempData["Message"] = "Your entry was succesfully updated!";
+
                 return RedirectToAction("Index");
             }
 
@@ -140,6 +144,8 @@ namespace Treehouse.FitnessFrog.Controllers
         {
             // Delete the entry
             _entriesRepository.DeleteEntry(id);
+
+            TempData["Message"] = "Your entry was succesfully deleted!";
 
             // Redirect to the entries list page
             return RedirectToAction("Index");
